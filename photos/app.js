d4168032f291ee912f6fcd11c189c3aca65c3d60
Ajay Shah <ajaygshah@yahoo.com>
2014-03-29 17:23:06 -0500

    Deleting js facebook

--- photos/app.js
+++ photos/app.js
@@ -5,31 +5,7 @@ $(document).ready(function() {
 		getphotos(tags);
 	})
 });
-$(".fb-login-button").click(function() {
-	$.ajaxSetup({ cache: true });
-	$.getScript('https://connect.facebook.net/en_UK/all.js', function(){
-		FB.init({
-			appId: '270182096483988'
-		});     
-		$('#loginbutton,#feedbutton').removeAttr('disabled');
-		FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-    // the user is logged in and has authenticated your
-    // app, and response.authResponse supplies
-    // the user's ID, a valid access token, a signed
-    // request, and the time the access token 
-    // and signed request each expire
-    var uid = response.authResponse.userID;
-    var accessToken = response.authResponse.accessToken;
-} else if (response.status === 'not_authorized') {
-    // the user is logged in to Facebook, 
-    // but has not authenticated your app
-} else {
-    // the user isn't logged in to Facebook.
-}
-});
-	});
-});
+
 
 var getphotos = function(tags) {
 	var photu = $("body").find("#show img");
