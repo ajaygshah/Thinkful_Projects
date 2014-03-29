22f87be4dab9f8f1eb5d86328d0257c66599c81d
Ajay Shah <ajaygshah@yahoo.com>
2014-03-29 17:13:33 -0500

    Changes

--- quiz/app.js
+++ quiz/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 
-    $("#btn").hide();
-    $("#corr, #incorr").hide();
-    
-    function questions(question, choices, answer) {
-       
-        this.question = question;
-        this.choices = choices;
-        this.answer = answer;
-        
-    }
-    
-    var question1 = new questions("Where is the Louvre Pyramid located?", ['United States of America','Scotland','Paris','New Zealand'],2);
-    var question2 = new questions("Where is the Icehotel located?", ['Dubai','Russia','London','Sweden'],3);
-    var question3 = new questions("If you're eating 'Pav Bhaji', which country are you in?", ['South Africa','India','Thailand','Germany'],1);
-    var question4 = new questions("If you're shopping internationally, which website should you use?",['Amazon.com','InternationalCheckout.com','TrueCurrency.com','Macys.com'],2) 
-    var listOfQuestions = [question1, question2, question3, question4];
+$("#btn").hide();
+$("#corr, #incorr").hide();
+ 
+function questions(question, choices, answer) {
+ 
+this.question = question;
+this.choices = choices;
+this.answer = answer;
+ 
+}
+ 
+var question1 = new questions("Where is the Louvre Pyramid located?", ['United States of America','Scotland','Paris','New Zealand'],2);
+var question2 = new questions("Where is the Icehotel located?", ['Dubai','Russia','London','Sweden'],3);
+var question3 = new questions("If you're eating 'Pav Bhaji', which country are you in?", ['South Africa','India','Thailand','Germany'],1);
+var question4 = new questions("If you're shopping internationally, which website should you use?",['Amazon.com','InternationalCheckout.com','TrueCurrency.com','Macys.com'],2) 
+var listOfQuestions = [question1, question2, question3, question4];
 
-//var bgimages = ['background.jpg','icehotel.jpg','pavbhaji.jpg','mall.jpg'];
-//var fontcolor = ['#EED2EE','black','white','#FFE1FF'];
-
+var bgimages = ['background.jpg','icehotel.jpg','pavbhaji.jpg','mall.jpg'];
+var fontcolor = ['#EED2EE','black','white','#FFE1FF'];
+               
 var i = 0;
 var j;
 var score = 0;
@@ -28,61 +28,79 @@ var num = 1;
 $("#head").text("Question " + num + " of " + listOfQuestions.length);
 
 $("#result").text("Score: " + score + " / " + listOfQuestions.length);
-
+ 
 function que(i){
-   
-    $("#que").append(listOfQuestions[i].question);
-    for(j=0; j <=listOfQuestions[i].choices.length-1; j++) {
-        $("#ans").append("<li>" + listOfQuestions[i].choices[j] + "</li>");
-    }
-    
+     
+        $("#que").append(listOfQuestions[i].question);
+        for(j=0; j <=listOfQuestions[i].choices.length-1; j++) {
+                $("#ans").append("<li>" + listOfQuestions[i].choices[j] + "</li>");
+        }
+      
 }
-
+ 
 que(i);
 
-
-$(document).on("click","li",function() {
-    if($(this).index() == listOfQuestions[i].answer) {
-     $("#corr").fadeIn();
-     score++;
-     advancequestion();
- }
- else {
-   $("#incorr").fadeIn();
-   
-   advancequestion();
-}
-
-})
-
-
-
-function advancequestion() {
-
-   $("#result").text("Score: " + score + " / " + listOfQuestions.length);
-   i++;
-   $("#que").empty();
-   $("#ans").empty();
-   if(num == listOfQuestions.length) {
-    $("#result").text("Total Score: " + score + " out of " + listOfQuestions.length);
-    comments();
-    $("#btn").show();
-    $("#btn").click(function() {
-        newgame();
-    })
-    
-}
-else {
+behave(i);
  
-    num++;
-    $("#head").text("Question " +num + " of " + listOfQuestions.length);
+function behave(i) {
+                $("li").click(function() {
+                                if($(this).index() == listOfQuestions[i].answer) {
+                                       $("#corr").fadeIn();
+                                        score++;
+                                        $("#result").text("Score: " + score + " / " + listOfQuestions.length);
+                                        i++;
+                                        $("#que").empty();
+                                        $("#ans").empty();
+                                        if(num == listOfQuestions.length) {
+                                                $("#result").text("Total Score: " + score + " out of " + listOfQuestions.length);
+                                                comments();
+                                                $("#btn").show();
+                                                $("#btn").click(function() {
+                                                    newgame();
+                                                })
+                                                
+                                        }
+                                        else {
+                                               
+                                                num++;
+                                                $("#head").text("Question " +num + " of " + listOfQuestions.length);
 
-    que(i);
-    $("#corr").fadeOut(2000);
-    
-    
-    
-}   
+                                                que(i);
+                                                 $("#corr").fadeOut(2000);
+                                                  
+                                                behave(i);
+                                                
+                                        }
+                                }
+                                else {
+                                         $("#incorr").fadeIn();
+                                         $("#result").text("Score: " + score + " / " + listOfQuestions.length);
+                                        i++;
+                                        $("#que").empty();
+                                        $("#ans").empty();
+                                       if(num == listOfQuestions.length) {
+                                                $("#result").text("Total Score: " + score + " out of " + listOfQuestions.length);
+                                                comments();
+                                                $("#btn").show();
+                                                $("#btn").click(function() {
+                                                    newgame();
+                                                })
+                                                
+                                        }
+                                        else {
+                                               
+                                                num++;
+                                                $("#head").text("Question " +num + " of " + listOfQuestions.length);
+
+                                                que(i);
+                                                 $("#incorr").fadeOut(2000);
+                                                behave(i);
+                                                
+                                        }
+                                }
+               
+                })
+ 
 }
 
 function comments() {
@@ -108,6 +126,7 @@ function newgame() {
 
     $("#result").text("Score: " + score + " / " + listOfQuestions.length);
     que(i);
+    behave(i);
 
 }
 
